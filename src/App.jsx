d198457d4df27e211fe920e105275c1576d9bba6
First-React-project <hbasuone@gamil.com>
2025-02-15 17:04:35 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,11 @@ import Type from "./components/Type/Type";
 import WishListed from "./components/WishListed/WishListed";
 import ForgetPassword from "./components/ForgetPassword/ForgetPassword";
 import ResetPassword from "./components/ResetPassword/ResetPassword";
-import Wishlistprovider from "./Context/Wishlistprovider";
+import Wishlistprovider from "./Context/WishlistProvider";
+
+
+
+
 let query = new QueryClient();
 
 let x = createBrowserRouter([
